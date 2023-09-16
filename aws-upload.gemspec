b4b037be5f627194820b0bdd347769a1524d47d7
Victor Maslov <nakilon@gmail.com>
2023-09-16 14:35:22 +0300

    aws-upload

--- aws-upload.gemspec
+++ aws-upload.gemspec
@@ -1,12 +1,14 @@
 Gem::Specification.new do |spec|
-  spec.name         = "nakicommon"
+  spec.name         = "nakicommon-aws-upload"
   spec.version      = "0.0.0"
-  spec.summary      = "common Ruby refinements, etc."
+  spec.summary      = "NOT FOR RUBYGEMS (yet?)"
   spec.metadata     = {"source_code_uri" => "https://github.com/nakilon/nakicommon"}
 
   spec.author       = "Victor Maslov aka Nakilon"
   spec.email        = "nakilon@gmail.com"
   spec.license      = "MIT"
 
-  # spec.files        = %w{ LICENSE nakicommon.gemspec lib/nakicommon.rb }
+  spec.add_dependency "aws-sigv4"
+
+  # spec.files        = %w{ aws-upload.gemspec lib/aws-upload.rb lib/flace.rb }
 end
---
+++ lib/aws-upload.rb
@@ -0,0 +1,23 @@
+require "net/http"
+require "aws-sigv4"
+module NakiCommon
+  def self.aws_upload payload, path, access_key_id, secret_access_key, header = {}
+    "https://storage.yandexcloud.net/#{path}".tap do |url|
+      code, response = ( ::Net::HTTP.new("storage.yandexcloud.net", 443).tap{ |_| _.use_ssl = true }.start do |http|
+        http.request ::Net::HTTP::Put.new(
+          url,
+          ::Aws.Sigv4.Signer.new(
+            service: "s3", region: "ru-central1",
+            access_key_id: access_key_id,
+            secret_access_key: secret_access_key
+          ).sign_request(http_method: "PUT", url: url, body: payload, headers: header).
+            headers.merge(header)
+        ).tap{ |_| _.body = payload }
+      end )
+      unless ["200", nil] == [code.code, response]
+        ::STDERR.puts code, response
+        fail "upload failed"
+      end
+    end
+  end
+end
