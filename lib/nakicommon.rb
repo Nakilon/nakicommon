ffbbb4ff3a00e8b65a18f1f735d968af942a2138
Victor Maslov <nakilon@gmail.com>
2024-07-26 17:47:37 +0300

    almost from ferrum_common

--- lib/nakicommon.rb
+++ lib/nakicommon.rb
@@ -1,52 +1,61 @@
 module NakiCommon
-  Error = ::Class.new ::RuntimeError
-  refine Array do
-    def assert_one
-      return at 0 if 1 == size
-      yield self if block_given?
-      raise Error, "size: #{size.to_s}"
-    end
-    def median
-      (sort[size / 2] + sort[(size - 1) / 2]) / 2r
-    end
-    def mean
-      reduce(:+)&.fdiv size
-    end
-  end
-  def self.shorter_backtrace
-    begin
-      yield
-    rescue
-      $!.set_backtrace( $!.backtrace_locations.chunk(&:path).map do |path, locs|
-        [path, locs.map(&:lineno).chunk(&:itself).map(&:first)].join ":"
-      end )
-      raise
-    end
-  end
-  def self.ratelimit seconds, filename
-    require "fileutils"
-    while 0 < t = ::File.mtime("#{filename}.touch") - ::Time.now + seconds
-      ::STDERR::puts "sleeping #{t} seconds (lock file: ./#{filename}.touch)"
-      sleep t
-    end if ::File.exist? "#{filename}.touch"
-    ::FileUtils.touch "#{filename}.touch"
-    yield
-  end
-  module Cache
-    def self.nethttputils url, id, dir = "cache_nethttputils"
-      require "base58"
-      filename = "#{dir}/#{::Base58.binary_to_base58 id.to_s.force_encoding "BINARY"}"
-      if ::File.exist? filename
-        ::File.binread filename
-      else
-        require "fileutils"
-        ::FileUtils.mkdir_p dir
-        require "nethttputils"
-        ::STDERR.puts "downloading #{url.to_s.inspect}"
-        ::NetHTTPUtils.request_data(url).tap do |_|
-          ::File.binwrite filename, _
-        end
-      end
+  # Error = ::Class.new ::RuntimeError
+  # refine Array do
+  #   def assert_one
+  #     return at 0 if 1 == size
+  #     yield self if block_given?
+  #     raise Error, "size: #{size.to_s}"
+  #   end
+  #   def median
+  #     (sort[size / 2] + sort[(size - 1) / 2]) / 2r
+  #   end
+  #   def mean
+  #     reduce(:+)&.fdiv size
+  #   end
+  # end
+  # def self.shorter_backtrace
+  #   begin
+  #     yield
+  #   rescue
+  #     $!.set_backtrace( $!.backtrace_locations.chunk(&:path).map do |path, locs|
+  #       [path, locs.map(&:lineno).chunk(&:itself).map(&:first)].join ":"
+  #     end )
+  #     raise
+  #   end
+  # end
+  # def self.ratelimit seconds, filename
+  #   require "fileutils"
+  #   while 0 < t = ::File.mtime("#{filename}.touch") - ::Time.now + seconds
+  #     ::STDERR::puts "sleeping #{t} seconds (lock file: ./#{filename}.touch)"
+  #     sleep t
+  #   end if ::File.exist? "#{filename}.touch"
+  #   ::FileUtils.touch "#{filename}.touch"
+  #   yield
+  # end
+  # module Cache
+  #   def self.nethttputils url, id, dir = "cache_nethttputils"
+  #     require "base58"
+  #     filename = "#{dir}/#{::Base58.binary_to_base58 id.to_s.force_encoding "BINARY"}"
+  #     if ::File.exist? filename
+  #       ::File.binread filename
+  #     else
+  #       require "fileutils"
+  #       ::FileUtils.mkdir_p dir
+  #       require "nethttputils"
+  #       ::STDERR.puts "downloading #{url.to_s.inspect}"
+  #       ::NetHTTPUtils.request_data(url).tap do |_|
+  #         ::File.binwrite filename, _
+  #       end
+  #     end
+  #   end
+  # end
+  def self.until_any timeout
+    ::Timeout.timeout timeout do
+      t = yield
+      raise Error unless t.respond_to? :each
+      return t unless t.empty?
+      sleep timeout * 0.1
+      redo
     end
   end
 end
